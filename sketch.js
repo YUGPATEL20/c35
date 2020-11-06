02d9f6d08e3f24d374896e6dcd540b48ec97e4e4
YUGPATEL20 <70703985+YUGPATEL20@users.noreply.github.com>
2020-11-06 13:47:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var ball , db;
+var position;
+
+function setup(){
+    db = firebase.database();
+    console.log(db);
+
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    var bPos = db.ref('ball/position');
+    console.log("bhpos : "+bPos) ;
+    bPos.on("value", readPosition , showError);
+}
+
+function draw(){
+    background("white");
+
+    if(position !== undefined){
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+}
+drawSprites();
+}
+
+function changePosition(x,y){
+
+db.ref('ball/position').set({
+    x : position.x+x,
+    y : position.y+y
+})
+
+    
+}
+
+function readPosition(data){
+    position = data.val();
+    console.log("position" + position);
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showError(){
+    console.log("Error");
+}
